a1a6ff6f0ab4b3cc74b54508809f7d3e82398f8d
KIMMIN5 <alsdh1111@naver.com>
2025-10-15 19:54:25 +0900

    feat: 예외 처리1, 2, 3 테스트 코드 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
@@ -12,7 +13,30 @@ class ApplicationTest extends NsTest {
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            assertThat(output()).contains("결과: 1");
+        });
+    }
+
+    @Test
+    void 예외1_구분자_없음() {
+        assertSimpleTest(() ->{
+            run("1");
+            assertThat(output().contains("1"));
+        });
+    }
+
+    @Test
+    void 예외2_빈문자열() {
+        assertSimpleTest(() -> {
+            run("");
+            assertThat(output().contains("0"));
+        });
+    }
+
+    @Test
+    void 예외3_숫자없는_입력값() {
+        assertSimpleTest(() -> {
+            assertThatIllegalArgumentException().isThrownBy(() -> run("abc"));
         });
     }
 
