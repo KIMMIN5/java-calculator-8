d2f3aa86a179a3df8d851bc4f56da4f0c87eca78
KIMMIN5 <alsdh1111@naver.com>
2025-10-15 19:51:42 +0900

    feat: 양수 추출

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,41 +1,62 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
 
-    public void startCalculator() {
+    public void Calculator() {
         System.out.print("덧셈할 문자열을 입력하세요: ");
         String userInput = Console.readLine();
-        separateInput(userInput);
+        String[] separateString = separateInput(userInput);
+        int[] numbers = extractNumbers(separateString);
     }
 
-    public String separateInput(String inputString) {
+    private String[] separateInput(String inputString) {
         String basicDelimiterRegex = ",|:";
-        String customDelimiterRegex = "//(.)\\\\n(.*)";
-        String extractString = "";
+        String customDelimiterRegex = "^//(.)\\\\n(.*)";
 
         Pattern pattern = Pattern.compile(customDelimiterRegex);
         Matcher matcher = pattern.matcher(inputString);
         if(matcher.find()) {
             String customDelimiter = matcher.group(1);
             basicDelimiterRegex += "|" + customDelimiter; // 기본구분자에 커스텀구분자 추가
-            extractString = Arrays.toString(matcher.group(2).split(basicDelimiterRegex));
+            return matcher.group(2).split(basicDelimiterRegex);
         }
 
         else { // 예외처리 #1 구분자가 없을 시
-            extractString = Arrays.toString(inputString.split(basicDelimiterRegex));
+            return inputString.split(basicDelimiterRegex);
         }
-        System.out.println("extractString: " + extractString);
-        return extractString;
+    }
+
+    private int[] extractNumbers(String[] extractString) throws IllegalArgumentException {
+        String numberRegex = "[0-9]+";
+        int[] extractNumbers = new int[extractString.length];
+        if(extractString.length == 0) { // 예외처리 #2 추출 할 숫자가 없을 시(빈문자열)
+            return new int[0];
+        }
+        else {
+            int number = 0;
+            int sum = 0;
+
+            for(int i=0; i<extractString.length; i++) {
+                if (extractString[i].matches(numberRegex)) {
+                    number = Integer.parseInt(extractString[i]);
+                    extractNumbers[i] = number;
+                    System.out.print(number);
+                }
+                else {
+                    throw new IllegalArgumentException("예외처리 #3: 추출 할 양수가 없습니다.");
+                }
+            }
+            return extractNumbers;
+        }
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application application = new Application();
-        application.startCalculator();
+        application.Calculator();
     }
 }
