f31790035e6aa32e983a52f4c95cb844d6dfe1d0
KIMMIN5 <alsdh1111@naver.com>
2025-10-16 22:22:23 +0900

    refactor: 초기, 분할, 추출 예외처리 강화

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,6 +9,9 @@ public class Application {
     public void Calculator() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String userInput = Console.readLine();
+        if(userInput.isEmpty()) {
+            System.out.println("결과 : " + 0);
+        }
         String[] separateString = separateInput(userInput);
         int[] numbers = extractNumbers(separateString);
         int sum = summateNumbers(numbers);
@@ -16,7 +19,7 @@ public class Application {
         System.out.println("결과 : " + sum);
     }
 
-    private String[] separateInput(String inputString) {
+    private String[] separateInput(String inputString) throws IllegalArgumentException {
         String basicDelimiterRegex = ",|:";
         String customDelimiterRegex = "^//(.)\\\\n(.*)";
 
@@ -24,40 +27,39 @@ public class Application {
         Matcher matcher = pattern.matcher(inputString);
         if(matcher.find()) {
             String customDelimiter = matcher.group(1);
-            basicDelimiterRegex += "|" + customDelimiter; // 기본구분자에 커스텀구분자 추가
+            if(customDelimiter.length() > 1) {
+                throw new IllegalArgumentException("커스텀 구분자는 여러 개를 설정할 수 없습니다.");
+            }
+            basicDelimiterRegex += "|" + customDelimiter; // 예외#2-2: 기본구분자에 커스텀구분자 추가
             return matcher.group(2).split(basicDelimiterRegex);
         }
 
-        else { // 예외처리 #1 구분자가 없을 시
-            return inputString.split(basicDelimiterRegex);
-        }
+        return inputString.split(basicDelimiterRegex);
     }
 
     private int[] extractNumbers(String[] extractString) throws IllegalArgumentException {
         String numberRegex = "^-?\\d+$"; // 모든 정수를 나타내는 정규표현식
         int[] extractNumbers = new int[extractString.length];
-        if(extractString.length == 1 && extractString[0].isEmpty()) { // 예외처리 #2 추출 할 숫자가 없을 시(빈문자열)
-            return new int[0];
-        }
-        else {
-            int number = 0;
-            int sum = 0;
 
-            for(int i=0; i<extractString.length; i++) {
-                if (extractString[i].matches(numberRegex)) {
-                    number = Integer.parseInt(extractString[i]);
-                    if(number < 0) {
-                        throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-                    }
-                    extractNumbers[i] = number;
+        int number = 0;
+        int sum = 0;
+
+        for(int i=0; i<extractString.length; i++) {
+            if (extractString[i].matches(numberRegex)) {
+                number = Integer.parseInt(extractString[i]);
+                if (number < 0) {
+                    throw new IllegalArgumentException("예외#3-1: 음수는 입력할 수 없습니다.");
                 }
-                else if(extractNumbers.length == 1 && extractNumbers[i] == 0) {
-                    throw new IllegalArgumentException("추출 할 양수가 없습니다.");
-                }
+                extractNumbers[i] = number;
             }
-            return extractNumbers;
+
+            if(!extractString[i].matches(numberRegex)) {
+                throw new IllegalArgumentException("예외#3-2: 숫자가 아닌 문자는 입력할 수 없습니다.");
+            }
         }
+        return extractNumbers;
     }
+
 
     private int summateNumbers(int[] numbers) {
         int sum = 0;
